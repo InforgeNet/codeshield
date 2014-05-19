9a6874bf025493df61331ff308b27dc18d957e54
Stefano Novelli <murdercode@gmail.com>
2014-05-19 18:18:38 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+For more informations about configuration please visit
+https://github.com/InforgeNet/codeshield/wiki
+
+*/
+
 $honeypot = 0; //Set 1 to Enable
 $honeypotapi = 'Type here your Honeypot Api Key http:BL'; //Need registration to http://www.projecthoneypot.org
 $maxserverload = '7'; //Define your Max Server Load. Check out your server for further informations 

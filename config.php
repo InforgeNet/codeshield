f23186eeda76ad8db8145a8a14553ed4882abf6f
Stefano Novelli <murdercode@gmail.com>
2014-05-26 15:00:41 +0200

    v 0.3
    Strengthen Cookie

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
 $salt = '$t3f4n091Li'; //Edit this line with a string you choose, please use a lot of special characters
-$method = 1; //Set 1 for Shared Hosting, set 2 for VPS/Dedicated
 $honeypot = 0; //Set 1 to Enable
 $honeypotapi = 'Type your API key of ProjectHoneyPot'; //Need registration to http://www.projecthoneypot.org/httpbl_configure.php
 $maxserverload = '8'; //Define your Max Server Load. Use 0 to filter ALL connections. My default value is 8. 
